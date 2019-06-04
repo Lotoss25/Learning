434da82745b697ac3bd9bbec872e80d4e310d379
Lotoss25 <lotoss25@gmail.com>
2019-06-04 12:38:36 +0300

    Les01 _ Les02

---
+++ Les_02/script.js
@@ -0,0 +1,14 @@
+"use strict";//строгий режим ;)
+
+let out = document.getElementById("out");
+let year = 1988;
+out.innerHTML = 2019 - year;
+alert("Andrew");
+//Выводим код
+console.log("let a = 'Hello'; let b = ' '; let c = 'world'; console.log(a+b+c); console.log(c+a+b);");
+//Выполняем код(может, нужно ;) )
+let a = 'Hello';
+let b = ' ';
+let c = 'world';
+console.log(a + b + c);
+console.log(c + a + b);
