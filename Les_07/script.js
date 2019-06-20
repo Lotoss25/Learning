b5abb10cb6bb9656958dcceccbcdb21d953b5c02
Lotoss25 <lotoss25@gmail.com>
2019-06-20 14:44:44 +0300

    Les_07

--- Les_07/script.js
+++ Les_07/script.js
@@ -174,3 +174,146 @@ signBtn.onclick = function () {
 }
 
 //Одинадцатое задание
+let spans = document.querySelectorAll("span");
+for (let i = 0; i < spans.length; i++) {
+    spans[i].style.background = "yellow";
+}
+
+//Двенадцатое задание
+let ps = document.querySelectorAll("p");
+document.querySelector("#paragraphs-span").innerHTML = ps.length;
+
+//Тринадцатое задание
+let arrayOfUnit = [1, 0, 0, 0, 0, 0];
+function out() {
+    document.querySelector("#output-array").innerHTML = "[" + arrayOfUnit + "]";
+}
+out();
+let i = 0
+document.querySelector("#btn-array").onclick = function () {
+
+    if (i < arrayOfUnit.length - 1) {
+        arrayOfUnit[i] = 0;
+        arrayOfUnit[i + 1] = 1;
+        i++;
+    }
+
+
+    out();
+}
+
+//Четырнадцатое задание
+function randomInteger(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+}
+let arrayOfNumbers = [];
+let num = 0;
+for (let i = 0; i < 5; i++) {
+    arrayOfNumbers[i] = randomInteger(-100, 100);
+    if (arrayOfNumbers[i] < 0) {
+        num += 1;
+        document.querySelector("#minnumbers-output").innerHTML = num;
+    }
+}
+document.querySelector("#arrayofnumbers-output").innerHTML = "[" + arrayOfNumbers + "]";
+
+//Пятнадцатое задание
+let arrayOfNumbers15 = [];
+for (let i = 0; i < 5; i++) {
+    arrayOfNumbers15[i] = randomInteger(-100, 100);
+    if (arrayOfNumbers15[i] < 0) {
+        document.querySelector("#minnumbers15-output").innerHTML += arrayOfNumbers15[i] + " ";
+    }
+}
+document.querySelector("#arrayofnumbers15-output").innerHTML = "[" + arrayOfNumbers15 + "]";
+
+//Шестнадцатое задание
+let arrayOfUnits = [];
+document.querySelector("#btn-arrayofunits").onclick = function () {
+    let arrayNum = +document.querySelector("#array-input").value;
+    for (let i = 0; i < arrayNum; i++) {
+        arrayOfUnits[i] = 1;
+    }
+    document.querySelector("#arrayofunits-span").innerHTML = "[" + arrayOfUnits + "]";
+}
+
+//Семнадцатое задание
+let randomArray = [];
+document.querySelector("#btn-randomarray").onclick = function () {
+    let arrayNum = +document.querySelector("#randomarray-input").value;
+    for (let i = 0; i < arrayNum; i++) {
+        arrayOfUnits[i] = randomInteger(0, 100);
+    }
+    document.querySelector("#randomarray-span").innerHTML = "[" + arrayOfUnits + "]";
+}
+
+//Восемнадцатое задание
+let arrayOfThings = [10, "20", 30, "40", "50", 60, "70", 80, 90];
+let secondArray = [];
+let j = 0;
+for (let i = 0; i < arrayOfThings.length; i++) {
+
+    if (typeof arrayOfThings[i] == "number") {
+        secondArray[j] = arrayOfThings[i];
+        j++;
+    }
+}
+document.querySelector("#array1-span").innerHTML = '[10, "20", 30, "40", "50", 60, "70", 80, 90]';
+document.querySelector("#array2-span").innerHTML = secondArray;
+
+//Девятнадцатое задание
+let arrayMax = [];
+let max = -100;
+for (let i = 0; i < 5; i++) {
+    arrayMax[i] = randomInteger(-100, 100);
+    if (arrayMax[i] > max) {
+        max = arrayMax[i];
+    }
+}
+document.querySelector("#arraymax1-span").innerHTML = arrayMax;
+document.querySelector("#arraymax2-span").innerHTML = max;
+
+//Двадцатое задание
+// создает массив, зполненный случайными числами от 1 до 10, а потом находит все повторяющиеся числа и выводит количество по каждому числу
+let arrayRepeat = [];
+let arrays = {};
+for (let i = 0; i < 20; i++) {
+    arrayRepeat[i] = randomInteger(0, 10);
+}
+//let m = 0;
+//то же самое что и indexOf, но еще работает для обьектов
+function objectIndexOf(myArray, secArray) {
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] === secArray) return 1;
+    }
+    return -1;
+}
+for (let i = 0; i < 20; i++) {
+    // проверяем нет ли повторов
+    if (objectIndexOf(arrays, arrayRepeat) == -1) {
+        //console.log(arrays.indexOf(arrayRepeat[i]));
+        let nums = 1;
+        for (let k = 0; k < 20; k++) {
+
+            if (arrayRepeat[i] === arrayRepeat[k]) {
+                if (i === k) {
+                    continue;
+                }
+                else {
+                    nums++;
+                }
+            }
+            arrays[arrayRepeat[i]] = nums;
+        }
+        //m++;
+    }
+    else {
+        continue;
+    }
+}
+document.querySelector("#arrayrepeat-span").innerHTML = "[" + arrayRepeat + "]";
+for (let key in arrays) {
+    document.querySelector("#arrayrepeat").innerHTML += "<p>" + key + " = " + arrays[key] + "</p>"
+}
