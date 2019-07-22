2ac9656e64af5f3146062473961fbc8ad69226b7
Lotoss25 <lotoss25@gmail.com>
2019-07-22 14:54:14 +0300

    Les_14

---
+++ Les_14/js/chips.js
@@ -0,0 +1,36 @@
+document.querySelector(".button").onclick = function () {
+    addChips("hello", 4000);
+}
+
+function addChips(message, sec = 3000) {
+    let chips = document.createElement("div");
+    chips.classList.add("chips");
+    chips.innerHTML = message;
+    chipsBody(chips);
+    //document.querySelector("body").appendChild(div);
+    setTimeout(function () { clearChips(chips) }, sec);
+
+}
+
+function clearChips(chips) {
+    chips.remove();
+    let rem = document.querySelectorAll(".chips");
+    if (rem.length === 0) {
+        document.querySelector(".chips-div").remove();
+    }
+
+}
+
+function chipsBody(chips) {
+    let chipsDiv = document.querySelector(".chips-div");
+    if (chipsDiv) {
+        chipsDiv.appendChild(chips);
+    }
+    else {
+        let chipsDiv = document.createElement("div");
+        chipsDiv.classList.add("chips-div");
+        document.querySelector("body").appendChild(chipsDiv);
+        chipsDiv.appendChild(chips);
+
+    }
+}
