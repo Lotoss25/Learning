5867f65326d43f34dd130d044ca4f0f93d63a60a
Lotoss25 <lotoss25@gmail.com>
2020-05-12 09:56:29 +0300

    1)

---
+++ Happiness/JS/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(".variable").slick({
+    infinite: true,
+    dots: true,
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    autoplay: true,
+});
